3196268636f02616dd1814c64048da2bcd0d77ae
mInternauta <minternauta@outlook.com>
2016-09-06 16:46:48 -0300

    Converted the Flat File System to a built-in module.

---
+++ DrawerBackup.StorageFileSystem/FlatFileSystem/FlatFileSystemModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawerBackup.StorageFileSystem.FlatFileSystem
+{
+    public class FlatFileSystemModule : IsfsModule
+    {
+        public Repository Open(string repositoryPath)
+        {
+            return new FlatRepository(repositoryPath);
+        }
+    }
+}
---
+++ DrawerBackup.StorageFileSystem/IsfsModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawerBackup.StorageFileSystem
+{
+    /// <summary>
+    /// Storage File System Module Interface
+    /// </summary>
+    public interface IsfsModule
+    {
+        /// <summary>
+        /// Open a Repository
+        /// </summary>
+        /// <param name="repositoryPath"></param>
+        /// <returns></returns>
+        Repository Open(string repositoryPath);
+    }
+}
